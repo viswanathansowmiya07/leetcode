bb882bb9037205a83ea65621ad789474120a63b0
viswanathansowmiya07 <viswanathansowmiya07@gmail.com>
2026-04-08 22:34:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (50.38%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,19 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int index = nums.length -1;
-        int swap =  nums.length -1;
-        int output =0;
-        for(int i = index ; i >=0;i--){
-
-            if(nums[i] == val){
-                int temp = nums[i];
-                nums[i] = nums[swap];
-                nums[swap]=temp;
-                swap--;
-            }else{
-                output++;
+        
+        int index =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[index]=nums[i];
+                index++;
             }
         }
-        return output;
+        return index;
     }
 }
